5defb832290446c262ceae2f82acc389f5ed7db1
Gustavo Gomes de Oliveira <gustavogomesdeoliveira@MacBook-Pro-de-Gustavo.local>
2017-05-11 11:00:20 -0300

    remove custom pin

--- FinalChallenge/CustomPin.swift
+++
@@ -1,9 +0,0 @@
-//
-//  CustomPin.swift
-//  FinalChallenge
-//
-//  Created by Daniel Dias on 11/05/17.
-//  Copyright © 2017 Gustavo Gomes de Oliveira. All rights reserved.
-//
-
-import Foundation
