dd6783afead5981989d6954e58ed4aae8a11c672
danieldias25 <daniel.dias.e@gmail.com>
2017-05-18 10:34:31 -0300

    new Popup class reference created

---
+++ FinalChallenge/AnnotationView.swift
@@ -0,0 +1,15 @@
+//
+//  AnnotationView.swift
+//  MapInformation
+//
+//  Created by Nishith Shah on 21/09/16.
+//  Copyright © 2016 Ramkrishna Sharma. All rights reserved.
+//
+
+import Foundation
+import MapKit
+
+class AnnotationView : MKAnnotationView {
+	
+	var mapPin: UIButton!
+}
---
+++ FinalChallenge/Popup.swift
@@ -0,0 +1,15 @@
+//
+//  Popup.swift
+//  MapInformation
+//
+//  Created by Nishith Shah on 22/09/16.
+//  Copyright © 2016 Ramkrishna Sharma. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Popup : UIViewController {
+	
+	
+}
