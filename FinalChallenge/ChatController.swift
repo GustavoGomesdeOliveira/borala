d0bd18522bf087f9d4e703194800200c3ca825ef
Gustavo Gomes de Oliveira <gustavogomesdeoliveira@MacBook-Pro-de-Gustavo.local>
2017-05-25 01:03:08 -0300

    chat view finished

--- FinalChallenge/ChatController.swift
+++ FinalChallenge/ChatController.swift
@@ -8,43 +8,134 @@
 
 import UIKit
 
-class ChatController: UIViewController {
+class ChatController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UITextFieldDelegate {
    
-    @IBOutlet weak var chatView: UIScrollView!
     @IBOutlet weak var navigation: UINavigationBar!
     
+    @IBOutlet weak var chatCollection: UICollectionView!
     @IBOutlet weak var navItem: UINavigationItem!
     
+    @IBOutlet weak var messageTextField: UITextField!
+    
+    var flagParaTeste = false
+    var chatConversation: ChatConversation!
+    
     let containerView =  UIView()
+    
+    
+   
     
     override func viewDidLoad() {
         
         super.viewDidLoad()
         
         self.navigation.topItem?.leftBarButtonItem = UIBarButtonItem(image: UIImage(named: "BackButton.png"), style: .plain, target: self, action: #selector(self.backAction))
+        
+        let stringArray = ["Teste", "Teste maior", "Teste maior ainda", "Teste maior que o anterior", "Teste maior de todos que meu deus que teste grande do caramba",]
+    
+        NotificationCenter.default.addObserver(self, selector: #selector(ChatController.keyboardWillShow), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(ChatController.keyboardWillHide), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+        
+        self.messageTextField.delegate = self
+        
+        self.chatCollection.alwaysBounceVertical = true
+        self.chatCollection.contentInset = UIEdgeInsets(top: 8, left: 0, bottom: 58, right: 0)
+        self.chatCollection.scrollIndicatorInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+
+        self.chatConversation = ChatConversation(message: stringArray)
+        
     }
 
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.chatConversation.message.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell:ChatMessageCell = collectionView.dequeueReusableCell(withReuseIdentifier: "chatCell", for: indexPath) as! ChatMessageCell
+        
+        
+        cell.textView.text = self.chatConversation.message[indexPath.row]
+        
+        setupCell(cell: cell)
+        
+        
+//        if mensagem.id == FIRAuth.auth()?.currentUser?.uid {
+//            
+//        }
+        
+        
+        cell.bubbleViewWidthAnchor?.constant = estimatedFrameForText(text: self.chatConversation.message[indexPath.row]).width + 20
+
+        flagParaTeste = !flagParaTeste
+        
+        return cell
+    }
+    
+    func setupCell(cell: ChatMessageCell){
+        if flagParaTeste {
+            
+            cell.bubbleView.backgroundColor = ChatMessageCell.blueColor
+            cell.textView.textColor = UIColor.white
+            cell.bubbleViewLeftAnchor?.isActive = false
+            cell.bubbleViewRightAnchor?.isActive = true
+            cell.profileImageChat.isHidden = true
+
+        } else {
+            
+            cell.bubbleView.backgroundColor = UIColor.lightGray
+            cell.textView.textColor = UIColor.black
+            cell.bubbleViewLeftAnchor?.isActive = true
+            cell.bubbleViewRightAnchor?.isActive = false
+            cell.profileImageChat.isHidden = false
+
+        }
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        //Default value
+        var heigth: CGFloat = 80
+        
+        //Get value from text
+        let text = self.chatConversation.message[indexPath.row]
+            
+        heigth = estimatedFrameForText(text: text).height + 20
+        
+        return CGSize(width: self.chatCollection.frame.width, height: heigth)
+        
+        
+    }
+    
+    private func estimatedFrameForText(text: String) -> CGRect{
+        
+        let size = CGSize(width: 200, height: 1000)
+        
+        let options = NSStringDrawingOptions.usesFontLeading.union(.usesLineFragmentOrigin)
+        
+        return NSString(string: text).boundingRect(with: size, options: options, attributes: [NSFontAttributeName:UIFont.systemFont(ofSize: 16)], context: nil)
+    }
+    
     
     @IBAction func send(_ sender: UIButton) {
         
-        let newMessage = UILabel(frame: CGRect(x: 100, y:  110, width: self.chatView.frame.width/2, height: 70))
-        newMessage.backgroundColor = UIColor.red
-        newMessage.text = "Messagem Um"
-        newMessage.layer.cornerRadius = 30
-        newMessage.layer.masksToBounds = true
-
-        
-        containerView.addSubview(newMessage)
-        
-        self.chatView.addSubview(containerView)
+        handleMessage()
         
     }
     
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        containerView.frame = CGRect(x: self.chatView.layer.position.x - 200, y: 100, width: chatView.contentSize.width, height: chatView.contentSize.height)
-        containerView.layer.cornerRadius = 30
-        //containerView.layer.masksToBounds = true
+    func handleMessage(){
+        
+        if (messageTextField.text?.characters.count)! > 0{
+            
+            let newMessage = self.messageTextField.text
+            
+            self.chatConversation.message.append(newMessage!)
+            
+            self.chatCollection.reloadData()
+            
+            self.messageTextField.text = nil
+            
+        }
     }
     
     func backAction() -> Void {
@@ -54,6 +145,40 @@ class ChatController: UIViewController {
         let nextViewController = storyBoard.instantiateViewController(withIdentifier: "TabBarVC") as! TabBarViewController
         self.present(nextViewController, animated:true, completion:nil)
         
+    }
+    
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        textField.resignFirstResponder()
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        handleMessage()
+        
+        textField.resignFirstResponder()
+        return true
+        
+    }
+    
+    func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+            if self.view.frame.origin.y == 0{
+                self.view.frame.origin.y -= keyboardSize.height
+            }
+        }
+    }
+    
+    func keyboardWillHide(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+            if self.view.frame.origin.y != 0{
+                self.view.frame.origin.y += keyboardSize.height
+            }
+        }
     }
 
     
---
+++ FinalChallenge/ChatMessageCell.swift
@@ -0,0 +1,99 @@
+//
+//  ChatMessageCell.swift
+//  FinalChallenge
+//
+//  Created by Gustavo Gomes de Oliveira on 24/05/17.
+//  Copyright © 2017 Gustavo Gomes de Oliveira. All rights reserved.
+//
+
+import UIKit
+
+class ChatMessageCell: UICollectionViewCell {
+    
+    let textView: UITextView = {
+        
+        let tv = UITextView()
+        tv.font = UIFont.systemFont(ofSize: 16)
+        tv.translatesAutoresizingMaskIntoConstraints = false
+        tv.backgroundColor = UIColor.clear
+        tv.textColor = UIColor.white
+        return tv
+    }()
+    
+    static let blueColor = UIColor(red: 0, green: 137, blue: 249, alpha: 1)
+    
+    let bubbleView: UIView = {
+        
+        let view = UIView()
+        view.backgroundColor = blueColor
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.layer.cornerRadius = 16
+        view.layer.masksToBounds = true
+
+        return view
+        
+    }()
+    
+    let profileImageChat: UIImageView = {
+       
+        let imageView = UIImageView()
+        imageView.image = #imageLiteral(resourceName: "profileImage")
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.layer.cornerRadius = 16
+        imageView.layer.masksToBounds = true
+        imageView.contentMode = .scaleAspectFill
+        
+        return imageView
+        
+    }()
+    
+    var bubbleViewWidthAnchor: NSLayoutConstraint?
+    var bubbleViewRightAnchor: NSLayoutConstraint?
+    var bubbleViewLeftAnchor: NSLayoutConstraint?
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        self.setup()
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)!
+        self.setup()
+
+    }
+    
+    func setup(){
+        
+        addSubview(bubbleView)
+        addSubview(textView)
+        addSubview(profileImageChat)
+        
+        profileImageChat.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 8).isActive = true
+        profileImageChat.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+        profileImageChat.widthAnchor.constraint(equalToConstant: 32).isActive = true
+        profileImageChat.heightAnchor.constraint(equalToConstant: 32).isActive = true
+
+        
+        bubbleViewWidthAnchor = bubbleView.widthAnchor.constraint(equalToConstant: 200)
+        bubbleViewWidthAnchor?.isActive = true
+        bubbleViewRightAnchor = bubbleView.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -8)
+        bubbleViewRightAnchor?.isActive = true
+        bubbleViewLeftAnchor = bubbleView.leftAnchor.constraint(equalTo: profileImageChat.rightAnchor, constant: 8)
+        
+        bubbleViewLeftAnchor?.isActive = false
+
+        bubbleView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        bubbleView.heightAnchor.constraint(equalTo: self.heightAnchor).isActive = true
+        
+        
+        
+        //textView.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
+        textView.leftAnchor.constraint(equalTo: bubbleView.leftAnchor, constant: 8).isActive = true
+        textView.rightAnchor.constraint(equalTo: bubbleView.rightAnchor).isActive = true
+        textView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        //textView.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        textView.heightAnchor.constraint(equalTo: self.heightAnchor).isActive = true
+    }
+    
+}
