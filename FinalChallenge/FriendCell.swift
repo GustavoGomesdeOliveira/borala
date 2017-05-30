8dac7fb531bd53d676a015e4176b40765b32d7c9
Gustavo Gomes de Oliveira <gustavogomesdeoliveira@MacBook-Pro-de-Gustavo.local>
2017-05-30 09:19:18 -0300

    friend list table created

---
+++ FinalChallenge/FriendCell.swift
@@ -0,0 +1,19 @@
+//
+//  FriendCell.swift
+//  FinalChallenge
+//
+//  Created by Gustavo Gomes de Oliveira on 30/05/17.
+//  Copyright © 2017 Gustavo Gomes de Oliveira. All rights reserved.
+//
+
+import UIKit
+
+class FriendCell: UITableViewCell {
+    
+    @IBOutlet weak var friendName: UILabel!
+    @IBOutlet weak var friendImage: UIImageView!
+    @IBOutlet weak var shadownLayer: UIView!
+    @IBOutlet weak var mainBackground: UIView!
+    
+    
+}
