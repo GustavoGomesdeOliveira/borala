9e06f5052469ee52503233e4fccd736dc8e11989
danieldias25 <daniel.dias.e@gmail.com>
2017-06-21 09:51:43 -0300

    choosefilter

---
+++ FinalChallenge/ChooseFilterController.swift
@@ -0,0 +1,37 @@
+//
+//  ChooseFilter.swift
+//  FinalChallenge
+//
+//  Created by Daniel Dias on 21/06/17.
+//  Copyright © 2017 Gustavo Gomes de Oliveira. All rights reserved.
+//
+
+import UIKit
+
+class ChooseFilterController: UIViewController {
+    
+    @IBOutlet weak var friends: UIButton!
+    @IBOutlet weak var newPeople: UIButton!
+    @IBOutlet weak var everyone: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+    }
+    
+    @IBAction func filterByFriends(_ sender: Any) {
+    }
+    
+    @IBAction func filterByNew(_ sender: Any) {
+    }
+    
+    
+    @IBAction func filterByEveryone(_ sender: Any) {
+    }
+    
+    
+    
+
+}
