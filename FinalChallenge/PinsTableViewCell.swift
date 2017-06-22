6aaa00b3836e637ac2dd6177324cbdd057148cb4
danieldias25 <daniel.dias.e@gmail.com>
2017-06-22 09:24:45 -0300

    Pins tableviewcontroller and xib file created

---
+++ FinalChallenge/PinsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PinsTableViewCell.swift
+//  FinalChallenge
+//
+//  Created by Daniel Dias on 22/06/17.
+//  Copyright © 2017 Gustavo Gomes de Oliveira. All rights reserved.
+//
+
+import UIKit
+
+class PinsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
