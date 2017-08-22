80b5fb5911009ff8b7117cb75e72edec7d4f1902
bergmpe <bergmpe@gmail.com>
2017-08-22 08:59:25 -0300

    added function to send a report email of abusive user.

--- functions/index.js
+++ functions/index.js
@@ -17,7 +17,55 @@
 
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const nodemailer = require('nodemailer');
+// Configure the email transport using the default SMTP transport and a GMail account.
+// For Gmail, enable these:
+// 1. https://www.google.com/settings/security/lesssecureapps
+// 2. https://accounts.google.com/DisplayUnlockCaptcha
+// For other types of transports such as Sendgrid see https://nodemailer.com/transports/
+// TODO: Configure the `gmail.email` and `gmail.password` Google Cloud environment variables.
+const gmailEmail = encodeURIComponent(functions.config().gmail.email);
+const gmailPassword = encodeURIComponent(functions.config().gmail.password);
+const mailTransport = nodemailer.createTransport(
+    `smtps://${gmailEmail}:${gmailPassword}@smtp.gmail.com`);
+// Your company name to include in the emails
+// TODO: Change this to your app or company name to customize the email sent.
+const APP_NAME = 'We love eat';
+
 admin.initializeApp(functions.config().firebase);
+
+// [START send Report User Email]
+/**
+ * Sends a welcome email to new user.
+ */
+// [START onWriteTrigger]
+exports.sendReportUserEmail = functions.database.ref('/reportedUsers/{dictionary}').onWrite(event => {
+// [END onWriteTrigger]
+  // [START eventAttributes]
+  //const user = event.data; // The Firebase user.
+  //const email = user.email; // The email of the user.
+  //const displayName = user.displayName; // The display name of the user.
+  const userId = event.data.key;
+  // [END eventAttributes]
+
+  return sendReportUserEmail(userId);
+});
+// [END send Report User Email]
+
+// Sends a report email to the admin.
+function sendReportUserEmail(userId) {
+  const mailOptions = {
+    from: `${APP_NAME} <noreply@firebase.com>`,
+    to: "we.love.eat@outlook.com"
+  };
+
+  // The user subscribed to the newsletter.
+  mailOptions.subject = `Report user to ${APP_NAME}!`;
+  mailOptions.text = `Hey the user ${userId || ''} was reported. Please verify.`;
+  return mailTransport.sendMail(mailOptions).then(() => {
+    console.log('New report email sent to:');
+  });
+}
 
 /**
  * Triggers when a user gets a new follower and sends a notification.
